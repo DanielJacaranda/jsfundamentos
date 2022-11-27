d29b8b78c19727ebb3c134151cb0416ee727542c
DanielJacaranda <danieljacaranda@me.com>
2022-11-27 19:19:23 -0300

    comparacoes template ternario

---
+++ comparacoes.js
@@ -0,0 +1,14 @@
+// == (comparação implícita)
+
+const numero = 5;
+const texto = "5";
+
+console.log(numero === texto);
+
+
+//typeof 
+console.log(typeof numero);
+console.log(typeof texto);
+
+// == compara só o valor
+// === compara valor e tipo de dados
---
+++ template-strings.js
@@ -0,0 +1,8 @@
+const nome = "Daniel";
+const idade = 2022 - 1983;
+const cidadeDeNascimento = "Recife";
+//const apresentacao = "Meu nome é " + nome + ", tenho " + idade + " anos e nasci na cidade de " + cidadeDeNascimento + "."; 
+
+const apresentacao = `Meu nome é ${nome}, tenho ${idade} anos e nasci na cidade de ${cidadeDeNascimento}.`;
+
+console.log(apresentacao);
